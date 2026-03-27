3119dcce0541c26e40c7c76af873a41150adfa5f
Subhasish Sahoo <subhasishsahooy@gmail.com>
2026-03-27 16:19:00 +0530

    trying again

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import LandingPage from './components/LandingPage/LandingPage';
-import LogOutBtn from './components/LoginBtn';
-import HomeBtn from './components/HomeBtn';
-import ContestBtn from './components/ContestBtn';
-import Home from './components/Home/Home';
-import Contests from './components/Contests/Contests';
 import { BrowserRouter, Outlet, Route, Routes, Navigate } from 'react-router-dom';
-import ProblemExplorer from './components/ProblemExplorer';
-import { AllTierData } from './Data/index';
-import Login from './components/Login/Login';
-import { useAuth } from './context/AuthContext';
+import { useAuth } from './context/AuthContext'; 
+import { AllTierData } from './Data/index'; 
+import LandingPage from './components/LandingPage/LandingPage';
+import Login from './components/Login/Login'; 
+import Home from './components/Home/Home'; 
+import Contests from './components/Contests/Contests'; 
+import ProblemExplorer from './components/ProblemExplorer'; 
+import LogOutBtn from './components/LoginBtn'; 
+import HomeBtn from './components/HomeBtn'; 
+import ContestBtn from './components/ContestBtn'; 
 
 const DashboardLayout = () => {
   return (
@@ -49,6 +49,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route 
           path="/" 
           element={user ? <Navigate to="/home" replace /> : <LandingPage />} 
@@ -57,6 +58,7 @@ function App() {
           path="/login" 
           element={user ? <Navigate to="/home" replace /> : <Login />} 
         />
+
         <Route element={user ? <DashboardLayout /> : <Navigate to="/login" replace />}>
           <Route path="/home" element={<Home />} />
           <Route path="/contests" element={<Contests />} />
@@ -68,6 +70,8 @@ function App() {
           <Route path="/tier1" element={<ProblemExplorer problemSet={AllTierData.tier1.data} infoIndex={AllTierData.tier1.infoIndex} />} />
           <Route path="/master" element={<ProblemExplorer problemSet={AllTierData.master.data} infoIndex={AllTierData.master.infoIndex} />} />
         </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
